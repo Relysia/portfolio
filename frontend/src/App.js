383ccc2ac6699f4b7747e1fa402330f33429852c
Riki Baranyai <baranyairiki@gmail.com>
2021-10-07 13:43:54 +0200

    Basic react setup

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './style/App.sass';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <h1>Hello</h1>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
